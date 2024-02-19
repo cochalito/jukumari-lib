c4f249c0ed1cd793aef2c4b6c69aa6f9b91072f4
Brayan Pereyra <brayan@processmaker.com>
2024-02-19 15:55:43 -0500

    Adicion de clase

---
+++ src/Classes/Api.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ProcessMaker\JukumariLib\Classes;
+
+class Api
+{
+    public function executeApi()
+    {
+        return array(
+            'url' => 'Mi url',
+            'endPoint' => 'mi end point'
+        );
+    }
+}
--- src/Jukumari.php
+++ src/Jukumari.php
@@ -2,6 +2,8 @@
 
 namespace ProcessMaker\JukumariLib;
 
+use ProcessMaker\JukumariLib\Classes\Api;
+
 class Jukumari
 {
     public function getInformation()
@@ -10,5 +12,11 @@ class Jukumari
             'Team' => 'Jukumari Team',
             'Author' => 'Brayan Pereyra (Cochalo)'
         );
+    }
+
+    public function getApi()
+    {
+        $api = new Api();
+        return $api->executeApi();
     }
 }
